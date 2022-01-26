36be301b32f23164aade946968a7a8ad1f1dca47
sakshi1299 <sakshikshatriya99@gmail.com>
2022-01-26 19:28:10 +0530

    solving protected project prob

--- js/checkPassword.js
+++ js/checkPassword.js
@@ -1,10 +1,14 @@
+const url_string = window.location.search;
+console.log(url_string.split("=")[1])
+// var url = new URL(url_string);
+const id = parseInt(url_string.split("=")[1]);
 function checkPass(){
     const password= document.getElementById('pass').value;
     //const text = password.value;
     console.log(password)
     if(password == "P0rtf0li0@S" || password == "matchbox")
     {
-        window.location.href = "projectsCaseStudy.html?id=" + 1;
+        window.location.href = "projectsCaseStudy.html?id=" + id;
     }
     else{
         alert("Invalid password");
